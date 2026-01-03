7063ce1904954443bd4370016d957615fd04297b
daniel-code20 <dgithub23@gmail.com>
2026-01-03 14:50:19 -0600

    layout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -8,7 +8,8 @@ type Props = {
 
 const AppLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-dvh flex flex-col overflow-x-hidden animate__animated animate__fadeIn ">
+    <div className="min-h-dvh flex flex-col overflow-x-hidden bg-gray-100 dark:bg-neutral-900
+        text-neutral-900 dark:text-neutral-100 animate__animated animate__fadeIn ">
       <Header />
       
       <main className="flex-1 px-4 py-8">
