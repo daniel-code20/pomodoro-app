9a467cdcd0f1da3bb66a4412ad0189314f1d673d
daniel-code20 <dgithub23@gmail.com>
2026-01-03 13:50:09 -0600

    button position

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,34 +1,33 @@
-import { useState } from "react"
-import { useTasks } from "../../hooks/useTasks"
-import TaskItem from "./TaskItem"
+import { useState } from "react";
+import { useTasks } from "../../hooks/useTasks";
+import TaskItem from "./TaskItem";
 
 const TaskList = () => {
-  const { tasks, addTask, toggleTask, deleteTask } = useTasks()
-  const [title, setTitle] = useState("")
+  const { tasks, addTask, toggleTask, deleteTask } = useTasks();
+  const [title, setTitle] = useState("");
 
   const handleAdd = () => {
-    if (!title.trim()) return
-    addTask(title)
-    setTitle("")
-  }
+    if (!title.trim()) return;
+    addTask(title);
+    setTitle("");
+  };
 
   return (
-      <div className="w-full flex flex-col gap-4">
-      <h3 className="text-sm font-semibold opacity-70">
-        Tasks
-      </h3>
+    <div className="w-full flex flex-col gap-4">
+      <h3 className="text-sm font-semibold opacity-70">Tasks</h3>
 
       {/* Input */}
-      <div className="flex gap-2 flex-wrap sm:flex-nowrap">
+      <div className="flex gap-2">
         <input
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="New task..."
           className="flex-1 min-w-0 px-3 py-2 text-sm rounded-lg bg-gray-200 dark:bg-zinc-800 outline-none"
         />
+
         <button
           onClick={handleAdd}
-          className="px-4 py-2 rounded-lg bg-neutral-800 text-white hover:bg-neutral-700 dark:bg-neutral-800 dark:text-white text-sm dark:hover:bg-neutral-700 transition w-full sm:w-auto"
+          className="px-4 py-2 rounded-lg bg-neutral-800 text-white hover:bg-neutral-700 dark:bg-neutral-800 dark:text-white text-sm dark:hover:bg-neutral-700 transition shrink-0 "
         >
           Add
         </button>
@@ -44,9 +43,7 @@ const TaskList = () => {
         "
       >
         {tasks.length === 0 && (
-          <p className="text-xs opacity-50 text-center">
-            No tasks yet
-          </p>
+          <p className="text-xs opacity-50 text-center">No tasks yet</p>
         )}
 
         {tasks.map((task) => (
@@ -59,7 +56,7 @@ const TaskList = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
